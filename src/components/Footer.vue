6df3830d5098b63131c1361415d53dbce1056488
Sae2611 <fuwafuwa355@gmai.com>
2022-08-14 21:25:55 +0900

    名前を変更しました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
+      <span class="names">○○○, さえ and ○○○</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
