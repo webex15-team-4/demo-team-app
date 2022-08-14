18e1294bb29366023235739365391a671b16177e
WataruSuenaga <watarus0202@icloud.com>
2022-08-14 21:26:58 +0900

    名前を付けました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
+      <span class="names">wataru suenaga, ○○○ and ○○○</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
